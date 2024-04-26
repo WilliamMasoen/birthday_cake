84c3cbf446d517c7477869b20e57914aa0879078
JustinAlianto <63857586+JustinAlianto@users.noreply.github.com>
2024-04-26 00:08:33 -0400

    Started styling for AddTextContainer

--- src/components/AddTextContainer.tsx
+++ src/components/AddTextContainer.tsx
@@ -1,5 +1,5 @@
 import "../styles/AddTextContainer.css";
-// import { useState } from "react";
+import { useState } from "react";
 
 let status = false;
 
@@ -9,17 +9,61 @@ export const showPopup = () => {
   console.log(status);
 };
 
+interface Content {
+  title: string;
+  description: string;
+}
+
 function AddTextContainer() {
+  const [status, setStatus] = useState(1);
+
+  const [Content, setContent] = useState<Content>({
+    title: "",
+    description: "",
+  });
+
   return (
     <>
       <div
         className={
-          status === false
-            ? "add-text-container-inactive"
-            : "add-text-container"
+          status === 1 ? "add-text-container" : "add-text-container-inactive"
         }
       >
-        Hello World
+        <form className="add-text-form">
+          <div className="add-text-title-container">
+            <label id="add-text-title-label">Title (required):</label>
+            <input
+              type="text"
+              name="add-text-title-input"
+              id="add-text-title-input"
+              placeholder="Happy Birthday, Will!"
+              value={Content.title}
+              required
+            />
+          </div>
+          <div className="add-text-content-container">
+            <input
+              type="text"
+              name="add-text-description-input"
+              id="add-text-description-input"
+              placeholder="Add Description (Optional)"
+              value={Content.description}
+            />
+          </div>
+          <div className="add-text-buttons-container">
+            <div className="add-text-cancel-button-container">
+              <button className="add-text-cancel-button">Cancel</button>
+            </div>
+            <div className="add-text-submit-button-container">
+              <input
+                type="submit"
+                name="add-text-submit-button"
+                id="add-text-submit-button"
+                value="Add Message!"
+              />
+            </div>
+          </div>
+        </form>
       </div>
     </>
   );
