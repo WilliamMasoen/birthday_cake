d237046d34ac1f8b0ff1db4d3e6c8ccc1b00a719
JustinAlianto <63857586+JustinAlianto@users.noreply.github.com>
2024-12-23 19:35:26 -0500

    Fixed confetti width height bug

--- src/components/Confetti.tsx
+++ src/components/Confetti.tsx
@@ -1,16 +1,22 @@
 import "../styles/Confetti.css";
 import React from "react";
 import Confetti from "react-confetti";
-import { useWindowSize } from "react-use";
 
 interface ConfettiComponentProps {
   isActive: boolean; // Prop to control confetti activation
 }
 
 const ConfettiComponent: React.FC<ConfettiComponentProps> = ({ isActive }) => {
-  const { width, height } = useWindowSize();
-
-  return <div>{isActive && <Confetti width={width} height={height} />}</div>;
+  return (
+    <div>
+      {isActive && (
+        <Confetti
+          width={window.innerWidth - 18}
+          height={document.body.scrollHeight}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ConfettiComponent;
